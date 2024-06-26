b47fa495b445b1035a23a985d7a71b81a2bec0ff
Ubuntu <ubuntu@ip-172-31-46-72.ap-south-1.compute.internal>
2024-06-26 14:01:10 +0000

    added -y for apt-get install

--- install-docker.sh
+++ install-docker.sh
@@ -4,9 +4,9 @@
 
     # Add Docker's official GPG key:
 
-      sudo apt-get update
-      sudo apt-get install ca-certificates curl
-      sudo install -m 0755 -d /etc/apt/keyrings
+      sudo apt-get update -y
+      sudo apt-get install ca-certificates curl -y
+      sudo install -m 0755 -d /etc/apt/keyrings 
       sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
       sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -17,13 +17,13 @@
            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-             sudo apt-get update
+             sudo apt-get update -y
 
 
 
 # 2) Install the Docker packages.
 
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 # 3) Verify that the Docker Engine installation is successful by running the hello-world image.
